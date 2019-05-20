07ae046b7071da12512ccd96162099fa64f2a963
Paul Traue <ptrauejr@gmail.com>
2019-05-19 21:54:44 -0500

    Adds show view and action

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,4 +1,5 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -32,6 +33,7 @@ class DocsController < ApplicationController
   private
 
   def find_doc
+    @doc = Doc.find(params[:id])
   end
 
   def doc_params
