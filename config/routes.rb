1f146198bd497d2bce8ecde0c4bafefd8f0eafb5
Paul Traue <ptrauejr@gmail.com>
2019-05-23 14:34:09 -0500

    Changes the root for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'
-
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticagted_root"
+  end
+
+  root 'welcome#index'
 end
